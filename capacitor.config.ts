73614fa01b0739632e8b76ed59c744b1ed6398b1
Styles322 <SUSANSHAMPEPE@GMAIL.COM>
2025-07-12 01:44:52 -0700

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dailycatholic.readings',
+  appName: 'Daily Catholic Readings',
+  webDir: 'dist'
+};
+
+export default config;
