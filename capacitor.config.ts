b4e5a1201f4a3bc26e5ae8743fe34e0f20e8f2b4
Styles322 <SUSANSHAMPEPE@GMAIL.COM>
2025-07-12 05:06:52 -0700

    Remove Capacitor and AdMob

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dailycatholic.readings',
-  appName: 'Daily Catholic Readings',
-  webDir: 'dist'
-};
-
-export default config;
